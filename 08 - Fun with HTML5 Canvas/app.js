96cc9ef73249bf5535a7bc3e5e99c56b5a4d057d
yunushan <yunushangoksu@gmail.com>
2023-12-17 10:24:17 +0300

    canvas working

---
+++ 08 - Fun with HTML5 Canvas/app.js
@@ -0,0 +1,28 @@
+const canvas = document.querySelector("#draw");
+const ctx = canvas.getContext("2d");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+ctx.strokeStyle = "#BADA55";
+ctx.lineJoin = "round";
+ctx.lineCap = "round";
+ctx.lineWidth = 50;
+
+let lastX = 0;
+let lastY = 0;
+let isDrawing = false;
+
+function draw(e) {
+  if (!isDrawing) return;
+  ctx.beginPath();
+  ctx.moveTo(lastX, lastY);
+  ctx.lineTo(e.offsetX, e.offsetY);
+  ctx.stroke();
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+}
+
+canvas.addEventListener("mousemove", draw);
+canvas.addEventListener("mousedown", (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener("mouseup", () => (isDrawing = false));
