0a727f66c3ba4802f25533e7b92ec95951795792
yunushangoksu <yunushangoksu@gmail.com>
2024-01-11 02:16:37 +0300

    finalized 23

--- 23 - Speech Synthesis/app.js
+++ 23 - Speech Synthesis/app.js
@@ -1,21 +1,31 @@
 const synth = window.speechSynthesis;
 const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
-const textArea = document.querySelector("textarea");
 const speakButton = document.querySelector("#speak");
 const stopButton = document.querySelector("#stop");
 let voices = [];
 
 function populateVoicelist() {
   voices = synth.getVoices();
+
+  for (i = 0; i < voices.length; i++) {
+    let voiceOption = document.createElement("option");
+    voiceOption.text = voices[i].name;
+    voiceOption.value = i;
+    voicesDropdown.add(voiceOption);
+  }
 }
 
 function speak() {
-  if (textArea.value) {
-    const msg = new SpeechSynthesisUtterance(textArea.value);
+  if (options[2].value) {
+    const msg = new SpeechSynthesisUtterance(options[2].value);
+    speechSynthesis.pause();
+    speechSynthesis.cancel();
     msg.pitch = options[1].value;
     msg.rate = options[0].value;
+    msg.voice = voices[voicesDropdown.value];
     synth.speak(msg);
+    console.log(msg.pitch, msg.rate, msg.voice);
   } else {
     return;
   }
